eb937341891f5c773554853dea8bb5988ea8d644
Arjun1932010 <83018389+Arjun1932010@users.noreply.github.com>
2022-05-21 12:12:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//https://teachablemachine.withgoogle.com/models/fyb3w31uf/
+
+Webcam.set({
+    height:300,
+    width:350,
+    image_format:"png",
+    png_quality:90
+});
+
+var camera= document.getElementById("camera");
+Webcam.attach(camera)
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='captured_image' src='"+data_uri+"'/>"
+    })
+};
+
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fyb3w31uf/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("Model is loaded");
+}
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if (error){
+     console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById('result_object_accuracy').innerHTML=results[0].confidence;
+        document.getElementById('result_object_name').innerHTML=results[0].label;
+    }
+}
